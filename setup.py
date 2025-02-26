53ac74a07b751aceb1b47e2283ae6d454af611c2
mark-hobbs <markhobbs91@gmail.com>
2025-02-26 19:20:22 +0000

    Refactoring

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyrf',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',],
+    author='Mark Hobbs',
+    author_email='markhobbs91@gmail.com',
+    description='A simple pure-Python package for generating spatially correlated random fields',
+    url='https://github.com/mark-hobbs/pyrf',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
